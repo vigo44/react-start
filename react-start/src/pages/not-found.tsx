1453657abb0bbb90cf1c7dc000a5769f7cd88939
vigo44 <vvigo@rambler.ru>
2023-03-19 20:56:14 +0400

    feat: add link to home in not-found-page

--- react-start/src/pages/not-found.tsx
+++ react-start/src/pages/not-found.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class NotFound extends React.Component {
   render(): React.ReactNode {
     return (
       <div>
-        <div>404</div>
+        <div>404 Page not found</div>
+        <div>
+          <Link to="/">Go Home</Link>
+        </div>
       </div>
     );
   }
