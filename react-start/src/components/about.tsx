286e4a01d9bddc6bdd3b67d3621c55b80561c475
vigo44 <vvigo@rambler.ru>
2023-03-20 01:12:13 +0400

    feat: add about

---
+++ react-start/src/components/about.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './about.css';
+
+class About extends React.Component {
+  render(): React.ReactNode {
+    return <div className="about">It&apos;s all about us</div>;
+  }
+}
+
+export default About;
--- react-start/src/pages/about-us.tsx
+++ react-start/src/pages/about-us.tsx
@@ -1,11 +1,13 @@
 import Header from '../components/header';
 import React from 'react';
+import About from '../components/about';
 
 class AboutUs extends React.Component {
   render(): React.ReactNode {
     return (
       <div>
         <Header />
+        <About />
       </div>
     );
   }
