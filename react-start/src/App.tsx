2be7c74535ab5e6200bcfd0260f3e5d07246fbaf
vigo44 <vvigo@rambler.ru>
2023-03-22 00:16:06 +0400

    feat: add rederect 404 page

--- react-start/src/App.tsx
+++ react-start/src/App.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import Home from './pages/home/home';
 import AboutUs from './pages/about-us/about-us';
 import NotFound from './pages/not-found/not-found';
@@ -11,7 +11,8 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="about" element={<AboutUs />} />
-          <Route path="*" element={<NotFound />} />
+          <Route path="404" element={<NotFound />} />
+          <Route path="*" element={<Navigate to={'/404'} />} />
         </Route>
       </Routes>
     </div>
