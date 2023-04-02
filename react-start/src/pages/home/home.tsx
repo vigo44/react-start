721138e5191fc4317ab5641632ac3a9a727b458b
vigo44 <vvigo@rambler.ru>
2023-04-02 21:41:39 +0400

    refactor: rewrote home into a functional component

--- react-start/src/pages/home/home.tsx
+++ react-start/src/pages/home/home.tsx
@@ -1,15 +1,12 @@
 import Search from '../../components/search/search';
-import React from 'react';
 import Cards from '../../components/cards/cards';
 
-class Home extends React.Component {
-  render(): React.ReactNode {
-    return (
-      <div>
-        <Search />
-        <Cards />
-      </div>
-    );
-  }
+function Home() {
+  return (
+    <div>
+      <Search />
+      <Cards />
+    </div>
+  );
 }
 export default Home;
