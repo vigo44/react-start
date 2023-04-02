59312e5d9e05c998120a574c6242060f698955eb
vigo44 <vvigo@rambler.ru>
2023-04-02 22:08:53 +0400

    refactor: rewrote search into a functional component with hooks

--- react-start/src/components/search/search.tsx
+++ react-start/src/components/search/search.tsx
@@ -1,39 +1,20 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 import './search.css';
 
-type MyState = { searchValue: string };
+function Search() {
+  const [searchValue, setSearchValue] = useState(localStorage.getItem('value') || '');
+  useEffect(() => {
+    localStorage.value = searchValue;
+  });
 
-class Search extends React.Component<Record<string, never>, MyState> {
-  constructor(props: Record<string, never>) {
-    super(props);
-    if (localStorage.getItem('value') !== null) {
-      this.state = {
-        searchValue: localStorage.value,
-      };
-    } else {
-      this.state = {
-        searchValue: '',
-      };
-    }
-  }
-
-  componentWillUnmount() {
-    localStorage.value = this.state.searchValue;
-  }
-
-  render() {
-    return (
-      <div className="search">
-        <form>
-          <input
-            value={this.state.searchValue}
-            onChange={(e) => this.setState({ searchValue: e.target.value })}
-          />
-          <button type="button">SEARCH</button>
-        </form>
-      </div>
-    );
-  }
+  return (
+    <div className="search">
+      <form>
+        <input value={searchValue} onChange={(e) => setSearchValue(e.target.value)} />
+        <button type="button">SEARCH</button>
+      </form>
+    </div>
+  );
 }
 
 export default Search;
