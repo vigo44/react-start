e4336206323e7161e5d29013859209fc3f5ea482
vigo44 <vvigo@rambler.ru>
2023-04-02 22:52:23 +0400

    refactor: card-component into a functional component

--- react-start/src/components/card/card.tsx
+++ react-start/src/components/card/card.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import './card.css';
 
 interface interfaceProduct {
@@ -15,34 +14,32 @@ interface interfaceProduct {
   images: string[];
 }
 
-class Card extends React.Component<interfaceProduct, Record<string, never>> {
-  render() {
-    return (
-      <div className="product">
-        <div className="product__wrapper-title">
-          <div className="product__title">{this.props.title}</div>
-          <div className="product__wrapper-category">
-            <div className="product__category">{this.props.category}</div>
-            <div className="product__brand">{this.props.brand}</div>
-          </div>
-        </div>
-        <div className="product__wrapper-img">
-          <img className="product__img" src={this.props.thumbnail} />
-        </div>
-        <div className="product__wrapper-rating">
-          <div className="product__rating">{this.props.rating}</div>
-          <div className="product__discount">{this.props.discountPercentage}</div>
-        </div>
-        <div className="product__wrapper-price">
-          <div className="product__price">{this.props.price}</div>
-          <div className="product__add"></div>
-        </div>
-        <div className="product__wrapper-stock">
-          <div className="product__stock">{this.props.stock}</div>
+function Card(props: interfaceProduct) {
+  return (
+    <div className="product">
+      <div className="product__wrapper-title">
+        <div className="product__title">{props.title}</div>
+        <div className="product__wrapper-category">
+          <div className="product__category">{props.category}</div>
+          <div className="product__brand">{props.brand}</div>
         </div>
       </div>
-    );
-  }
+      <div className="product__wrapper-img">
+        <img className="product__img" src={props.thumbnail} />
+      </div>
+      <div className="product__wrapper-rating">
+        <div className="product__rating">{props.rating}</div>
+        <div className="product__discount">{props.discountPercentage}</div>
+      </div>
+      <div className="product__wrapper-price">
+        <div className="product__price">{props.price}</div>
+        <div className="product__add"></div>
+      </div>
+      <div className="product__wrapper-stock">
+        <div className="product__stock">{props.stock}</div>
+      </div>
+    </div>
+  );
 }
 
 export default Card;
