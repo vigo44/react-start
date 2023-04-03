8d320f60c09c75aa873c2da19382782f4af2710a
vigo44 <vvigo@rambler.ru>
2023-04-03 08:52:03 +0400

    refactor: rewrite form-page into a functional component with hooks

--- react-start/src/pages/form-page/form-page.tsx
+++ react-start/src/pages/form-page/form-page.tsx
@@ -1,14 +1,10 @@
-import React from 'react';
+import { useState } from 'react';
 import Form from '../../components/form/form';
 import CardsUser from '../../components/cards-user/cards-user';
 import ModalWindows from '../../components/modal-window/modal-window';
 import './form-page.css';
 
-interface Props {
-  children?: React.ReactNode;
-}
-
-interface userCard {
+interface UserCard {
   user: string;
   date: string;
   continent: string;
@@ -18,35 +14,26 @@ interface userCard {
   accept: boolean;
 }
 
-interface FormState {
-  cards: userCard[];
-  modalOpen: boolean;
-}
+function FormPage() {
+  const [cards, setCards] = useState([] as UserCard[]);
+  const [modalOpen, setModal] = useState(false);
 
-class FormPage extends React.Component<Props, FormState> {
-  formRef: React.RefObject<HTMLFormElement> = React.createRef();
-
-  constructor(props: Props) {
-    super(props);
-    this.state = { cards: [], modalOpen: false };
-  }
-
-  updateCards = (card: userCard) => {
-    this.setState({ cards: [...this.state.cards, card], modalOpen: true });
+  const updateCards = (card: UserCard) => {
+    setCards([...cards, card]);
+    setModal(true);
   };
 
-  closeModal = () => {
-    this.setState({ modalOpen: false });
+  const closeModal = () => {
+    setModal(false);
   };
 
-  render(): React.ReactNode {
-    return (
-      <div className="wrapperForm">
-        <Form updateCards={this.updateCards} />
-        <CardsUser cards={this.state.cards} />
-        <ModalWindows isOpen={this.state.modalOpen} close={this.closeModal} />
-      </div>
-    );
-  }
+  return (
+    <div className="wrapperForm">
+      <Form updateCards={updateCards} />
+      <CardsUser cards={cards} />
+      <ModalWindows isOpen={modalOpen} close={closeModal} />
+    </div>
+  );
 }
+
 export default FormPage;
