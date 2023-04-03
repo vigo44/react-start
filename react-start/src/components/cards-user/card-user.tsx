b2600bb84b1ed9b6b7809169295609ed633a70cc
vigo44 <vvigo@rambler.ru>
2023-04-03 07:32:51 +0400

    refactor: rewrite card-user into a functional component

--- react-start/src/components/cards-user/card-user.tsx
+++ react-start/src/components/cards-user/card-user.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import './card-user.css';
 
 interface Props {
@@ -10,24 +9,15 @@ interface Props {
   avatar: string;
 }
 
-export default class CardUser extends React.Component<Props> {
-  constructor(props: Props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div className="card-user">
-        <div
-          className="card-user__avatar"
-          style={{ backgroundImage: `url(${this.props.avatar})` }}
-        ></div>
-        <div className="card-user__name">Name: {this.props.user}</div>
-        <div className="card-user__data">Was born:{this.props.date}</div>
-        <div className="card-user__continent">From: {this.props.continent}</div>
-        <div className="card-user__gender">Gender: {this.props.gender}</div>
-        <div className="card-user__sent">News: {this.props.send ? 'sent' : `don't sent`}</div>
-      </div>
-    );
-  }
+export default function CardUser(props: Props) {
+  return (
+    <div className="card-user">
+      <div className="card-user__avatar" style={{ backgroundImage: `url(${props.avatar})` }}></div>
+      <div className="card-user__name">Name: {props.user}</div>
+      <div className="card-user__data">Was born:{props.date}</div>
+      <div className="card-user__continent">From: {props.continent}</div>
+      <div className="card-user__gender">Gender: {props.gender}</div>
+      <div className="card-user__sent">News: {props.send ? 'sent' : `don't sent`}</div>
+    </div>
+  );
 }
