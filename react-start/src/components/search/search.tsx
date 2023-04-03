92363e409a4c5dfe4fac26d56e225837568f57f4
vigo44 <vvigo@rambler.ru>
2023-04-03 19:34:46 +0400

    fix: add save search-value when components is unmounted

--- react-start/src/components/search/search.tsx
+++ react-start/src/components/search/search.tsx
@@ -1,18 +1,24 @@
-import { useEffect, useState } from 'react';
+import { ChangeEvent, useEffect, useRef, useState } from 'react';
 import './search.css';
 
 function Search() {
   const [searchValue, setSearchValue] = useState(localStorage.getItem('value') || '');
+  const valueRef = useRef<string>(searchValue);
+  const handlerInput = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearchValue(e.target.value);
+    valueRef.current = e.target.value;
+  };
+
   useEffect(() => {
     return () => {
-      localStorage.value = searchValue;
+      localStorage.value = valueRef.current;
     };
-  });
+  }, []);
 
   return (
     <div className="search">
       <form>
-        <input value={searchValue} onChange={(e) => setSearchValue(e.target.value)} />
+        <input value={searchValue} onChange={handlerInput} />
         <button type="button">SEARCH</button>
       </form>
     </div>
