6abb7769826fac1d2b0cc5a780b15cf1e272b639
vigo44 <vvigo@rambler.ru>
2023-04-03 08:06:01 +0400

    refactor: rewrite modal-window into a functional component

--- react-start/src/components/modal-window/modal-window.tsx
+++ react-start/src/components/modal-window/modal-window.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import './modal-window.css';
 
 interface Props {
@@ -6,27 +5,20 @@ interface Props {
   close: () => void;
 }
 
-export default class ModalWindow extends React.Component<Props> {
-  constructor(props: Props) {
-    super(props);
-  }
-  render() {
-    return (
-      <div className={`modal-windows ${this.props.isOpen ? '' : 'modal-windows_invisible'}`}>
-        <div
-          className={`modal-windows__shadow ${
-            this.props.isOpen ? '' : 'modal-windows__shadow_invisible'
-          }`}
-        ></div>
-        <div
-          className={`modal-windows__content ${
-            this.props.isOpen ? '' : 'modal-windows__content_invisible'
-          }`}
-        >
-          <p className="modal-windows__text">User is saved!</p>
-          <input type="button" value="OK" onClick={this.props.close} />
-        </div>
+export default function ModalWindow(props: Props) {
+  return (
+    <div className={`modal-windows ${props.isOpen ? '' : 'modal-windows_invisible'}`}>
+      <div
+        className={`modal-windows__shadow ${props.isOpen ? '' : 'modal-windows__shadow_invisible'}`}
+      ></div>
+      <div
+        className={`modal-windows__content ${
+          props.isOpen ? '' : 'modal-windows__content_invisible'
+        }`}
+      >
+        <p className="modal-windows__text">User is saved!</p>
+        <input type="button" value="OK" onClick={props.close} />
       </div>
-    );
-  }
+    </div>
+  );
 }
