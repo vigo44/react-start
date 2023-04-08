f438aae73fcbcdeffa3513af030c62e9c6fef484
vigo44 <vvigo@rambler.ru>
2023-04-09 00:56:35 +0400

    feat: add pagination

--- react-start/src/components/cards/cards.tsx
+++ react-start/src/components/cards/cards.tsx
@@ -7,15 +7,20 @@ function Cards() {
   const [cardsData, setCardsData] = useState<interfaceCharacter[]>([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState('');
+  const [path, setPatch] = useState('https://rickandmortyapi.com/api/character');
+  const [prevPage, setPrevPage] = useState(null);
+  const [nextPage, setNextPage] = useState(null);
 
-  async function fetchCards() {
+  async function fetchCards(path: RequestInfo | URL) {
     try {
       setError('');
       setLoading(true);
-      const response = await fetch('https://rickandmortyapi.com/api/character');
+      const response = await fetch(path);
       if (response.ok) {
         const jsonData = await response.json();
         setCardsData(jsonData.results);
+        setPrevPage(jsonData.info.prev);
+        setNextPage(jsonData.info.next);
       } else {
         const errorFetch = new Error(`Network Error: response ${response.status}`);
         throw errorFetch;
@@ -28,19 +33,43 @@ function Cards() {
   }
 
   useEffect(() => {
-    fetchCards();
-  }, []);
+    fetchCards(path);
+  }, [path]);
 
   return (
-    <div className="wrapper">
-      {loading && <p>Loading...</p>}
-      {error && <p>{error}</p>}
-      <div className="characters">
-        {cardsData.map((item) => {
-          return <Card key={item.id} {...item} />;
-        })}
+    <>
+      <div className="wrapper-button">
+        <input
+          className={'button ' + (prevPage == null ? 'button_inactive' : '')}
+          type="button"
+          value={'<<'}
+          onClick={() => {
+            if (prevPage) {
+              setPatch(prevPage);
+            }
+          }}
+        />
+        <input
+          className={'button ' + (nextPage == null ? 'button_inactive' : '')}
+          type="button"
+          value={'>>'}
+          onClick={() => {
+            if (nextPage) {
+              setPatch(nextPage);
+            }
+          }}
+        />
       </div>
-    </div>
+      <div className="wrapper">
+        {loading && <p>Loading...</p>}
+        {error && <p>{error}</p>}
+        <div className="characters">
+          {cardsData.map((item) => {
+            return <Card key={item.id} {...item} />;
+          })}
+        </div>
+      </div>
+    </>
   );
 }
 
