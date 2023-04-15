b3cc1cf638e434a5f820f5d84fc2beebd1c56e81
vigo44 <vvigo@rambler.ru>
2023-04-15 17:39:50 +0400

    feat: change test for card-description-component

--- react-start/src/components/card-description/card-description.test.tsx
+++ react-start/src/components/card-description/card-description.test.tsx
@@ -2,6 +2,8 @@ import createFetchMock from 'vitest-fetch-mock';
 import { vi } from 'vitest';
 import cartsData from '../../assets/data/card-data';
 import { render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { store } from '../../store/index';
 
 import CardDescription from './card-discription';
 
@@ -17,7 +19,11 @@ describe('testing card-description', () => {
   it('calls returns data to me', async () => {
     fetchMocker.mockResponseOnce(JSON.stringify(cartsData.results[0]));
 
-    render(<CardDescription link="https://rickandmortyapi.com/api/character/1" />);
+    render(
+      <Provider store={store}>
+        <CardDescription />
+      </Provider>
+    );
     //ACT
     //EXPEXT
     await screen.findByText(/rick sanchez/i);
